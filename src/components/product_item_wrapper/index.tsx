c1e1bcb1ccc6933c05ea8250dda4989802545d40
Alice Sousa <alicesinhasousa7@gmail.com>
2024-06-17 15:03:15 -0300

    fix: adding a temporary any

--- src/components/product_item_wrapper/index.tsx
+++ src/components/product_item_wrapper/index.tsx
@@ -7,10 +7,9 @@ import { truncateText } from '@/utils/truncateText'
 import Image from 'next/image'
 import { TrashIcon, PlusCircleIcon } from '@heroicons/react/20/solid'
 import { IconButtonStyled } from '../icon_button'
-import { IProductItem } from '@interfaces/product'
 
 interface ProductItemProps {
-	itemData: IProductItem | ICardStore
+	itemData: any | ICardStore
 }
 export default function ProductCard({ itemData }: ProductItemProps) {
 	const {
